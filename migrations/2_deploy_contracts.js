290884d026b35441f3ea73d3d698ed9f50a6f635
BerkeAnna <berke.anna0803@gmail.com>
2023-11-28 12:13:52 +0100

    így most lkét contract van, és form jó

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const GemstoneExtraction = artifacts.require("GemstoneExtraction");
+const GemstoneSelecting = artifacts.require("GemstoneSelecting");
 
 module.exports = function(deployer) {
   deployer.deploy(GemstoneExtraction);
+  deployer.deploy(GemstoneSelecting);
 };
