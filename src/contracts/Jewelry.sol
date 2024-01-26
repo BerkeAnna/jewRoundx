0d5f92604977479dde0cc902260349f9e04655a7
BerkeAnna <berke.anna0803@gmail.com>
2024-01-26 09:00:18 +0100

    jewelry making function + struct

--- src/contracts/Jewelry.sol
+++ src/contracts/Jewelry.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Jewelry {
 
-    struct GoldData{
+    /*struct GoldData{
         string name;
         uint gemId;
         uint goldCarat;
@@ -19,6 +19,52 @@ contract Jewelry {
         string name;
         uint gemId;
 
+    }*/
+
+    uint public jewelryCount = 0;
+    mapping( uint => JewelryData) public jewelry;
+
+    struct JewelryData{
+        uint id;
+        string name;
+        uint gemId;
+        string metal;
+        uint depth;
+        uint height;
+        uint width;
+        uint size; //mekkora ujjra valo pl
+        uint date;
+        bool sale;
+        uint price;
+        address payable jeweler;
+        address payable owner;
     }
+
+    event JewelryMaking(
+        uint id,
+        string name,
+        uint gemId,
+        string metal,
+        uint depth,
+        uint height,
+        uint width,
+        uint size,
+        uint date,
+        bool sale,
+        uint price,
+        address payable jeweler,
+        address payable owner
+    );
+
+    function jewelryMaking(string memory _name, uint _gemId, string memory _metal, uint _depth, uint _height, uint _width, uint _size, uint _date, bool _sale, uint _price ) public {
+         
+        jewelryCount++;
+       jewelry[jewelryCount] = JewelryData(jewelryCount, _name, _gemId, _metal, _depth, _height, _width, _size, _date, _sale, _price, msg.sender, msg.sender);
+
+       emit JewelryMaking(jewelryCount, _name, _gemId, _metal, _depth, _height, _width, _size, _date, _sale, _price, msg.sender, msg.sender);
+
     
+    }
+
+ 
 }
