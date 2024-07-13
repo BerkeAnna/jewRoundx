5c827c04e09476967555f110afc0293d722969f1
BerkeAnna <berke.anna0803@gmail.com>
2024-07-13 13:21:30 +0200

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,26 +17,46 @@ const Profile = ({ userInfo, ownedJewelryCount, cuttedGemCount, ownedMinedGemCou
   };
 
   return (
-    <div className='pt-5'>
-      <h1>Profile page in process....</h1>
+    <div className='pt-5' style={{ textAlign: 'center' }}>
       <h1>Hi {userInfo ? userInfo.username : 'xy'}!</h1>
-      <h2>TODO:</h2>
-      <p>-metamaskból adatok</p>
-      <p>address: {userInfo ? userInfo.address : ''}</p>
-      <p>name: {userInfo ? userInfo.username : ''}</p>
-      <p>Currently owned jewelry: {ownedJewelryCount}</p>
-      <p>Owned made jewelry: {ownedMadeJewelryCount}</p>
-      <p>Cutted Gem Count: {cuttedGemCount}</p>
-      <p>All the mined gem mined so far: {ownedMinedGemCount}</p>
-      <p>- név, miből mennyi van jelenleg</p>
-      <p>- gomb a saját gems oldalra irányításhoz </p>
-      {userInfo && (
-        <div>
-          <p>Address: {userInfo.address}</p>
-          <p>Username: {userInfo.username}</p>
-          <p>Role: {userInfo.role}</p>
-        </div>
-      )}
+     
+      <div className="card" style={{ 
+        marginBottom: '20px', 
+        padding: '20px', 
+        backgroundColor: '#FFF7F3', // kékses árnyalatú háttér
+        width: '90%', 
+        margin: 'auto', 
+        textAlign: 'center', 
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' // shadow
+      }}>
+      <h3>Your data:</h3>
+        {userInfo && (
+          <div>
+            <p>Address: {userInfo.address}</p>
+            <p>Username: {userInfo.username}</p>
+            <p>Role: {userInfo.role}</p>
+          </div>
+        )}
+      </div>
+
+      <div className="card" style={{ 
+        marginBottom: '20px', 
+        padding: '20px', 
+        backgroundColor: '#FFF7F3', // kékses árnyalatú háttér
+        width: '90%', 
+        margin: 'auto', 
+        textAlign: 'center', 
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', // shadow
+        marginTop: '20px' // add this line to create 20px space between cards
+      }}>
+        <h3>Gems summary data:</h3>
+        <p>All the mined gem mined so far: {ownedMinedGemCount}</p>
+        <p>Cutted Gem Count: {cuttedGemCount}</p>
+        <p>Owned made jewelry: {ownedMadeJewelryCount}</p>
+        <p>Currently owned jewelry: {ownedJewelryCount}</p>
+      </div>
+     
+    
     </div>
   );
 };
