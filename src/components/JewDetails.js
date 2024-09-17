7b065bf3a18f7bb633a69033e9ab127abe131a96
BerkeAnna <berke.anna0803@gmail.com>
2024-09-17 16:39:07 +0200

    ok

--- src/components/JewDetails.js
+++ src/components/JewDetails.js
@@ -3,6 +3,12 @@ import { useParams } from 'react-router-dom';
 
 function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract, gemstoneSelectingContract, gemstoneExtractionContract }) {
   const { id } = useParams();
+  const gemId = id;
+
+  const gemSelected = selectedGems.filter(gem => gem.owner && gem.id == gemId);
+  const minedGem = minedGems.filter(gem => gem.owner && gem.id == gemId);
+  const jewelryDetails = jewelry.filter(item => item.id == gemId);
+
   const [prevGemsArray, setPrevGemsArray] = useState([]);
   const [filteredJewelryEvents, setFilteredJewelryEvents] = useState([]);
   const [filteredSelectedGemEvents, setFilteredSelectedGemEvents] = useState([]);
@@ -19,20 +25,22 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
   useEffect(() => {
     const fetchJewelryDetails = async () => {
       try {
+        // Jewelry részletek lekérése
         const details = await jewelryContract.methods.getJewelryDetails(id).call();
         const gemIdsAsInt = details.previousGemIds.map(gemId => parseInt(gemId.toString(), 10));
         setPrevGemsArray(gemIdsAsInt);
 
+        // Fetch events
         const jewelryEvents = await jewelryContract.getPastEvents('allEvents', {
           fromBlock: 0,
-          toBlock: 'latest',
+          toBlock: 'latest'
         });
         const filteredJewelry = jewelryEvents.filter(event => parseInt(event.returnValues.id) === parseInt(id));
         setFilteredJewelryEvents(filteredJewelry);
 
         const selectedGemEvents = await gemstoneSelectingContract.getPastEvents('allEvents', {
           fromBlock: 0,
-          toBlock: 'latest',
+          toBlock: 'latest'
         });
         const filteredSelectedGems = selectedGemEvents.filter(event =>
           gemIdsAsInt.includes(parseInt(event.returnValues.id))
@@ -41,13 +49,14 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
 
         const minedGemEvents = await gemstoneExtractionContract.getPastEvents('allEvents', {
           fromBlock: 0,
-          toBlock: 'latest',
+          toBlock: 'latest'
         });
         const filteredMinedGems = minedGemEvents.filter(event =>
           gemIdsAsInt.includes(parseInt(event.returnValues.id))
         );
         setFilteredMinedGemEvents(filteredMinedGems);
 
+        // Fetch transaction dates for all events' block numbers
         const allEvents = [...filteredJewelry, ...filteredSelectedGems, ...filteredMinedGems];
         const blockNumbers = allEvents.map(event => event.blockNumber);
         const uniqueBlockNumbers = [...new Set(blockNumbers)];
@@ -57,6 +66,7 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
           blockDatesMap[blockNumber] = await getTransactionDate(window.web3, blockNumber);
         }
         setBlockDates(blockDatesMap);
+
       } catch (error) {
         console.error('Error fetching details:', error);
       }
@@ -129,8 +139,10 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
         <p><strong>Mining Location:</strong> {gem.miningLocation}</p>
         <p><strong>Mining Year:</strong> {gem.miningYear.toString()}</p>
         <p><strong>Extraction Method:</strong> {gem.extractionMethod}</p>
+        <p><strong>Selected:</strong> {gem.selected.toString()}</p>
         <p><strong>Price:</strong> {window.web3.utils.fromWei(gem.price.toString(), 'Ether')} Eth</p>
         <p><strong>Miner:</strong> {gem.owner}</p>
+
         <hr />
         <h3>Transaction Details</h3>
         {renderTransactionDetails(filteredMinedGemEvents, gem.id)}
@@ -166,13 +178,15 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
         <p><strong>Price:</strong> {window.web3.utils.fromWei(gem.price.toString(), 'Ether')} Eth</p>
         <p><strong>Gem cutter:</strong> {gem.gemCutter}</p>
         <p><strong>Owner:</strong> {gem.owner}</p>
+        <p><strong>previousGemId:</strong> {gem.previousGemId.toString()}</p>
+
         <hr />
         <h3>Transaction Details</h3>
         {renderTransactionDetails(filteredSelectedGemEvents, gem.id)}
       </div>
     ));
   };
-  
+ 
 
   // Circular navigation logic based on the filtered list of gems related to the current jewelry
   const filteredMinedGems = minedGems.filter(gem => prevGemsArray.includes(parseInt(gem.id, 10))).reverse();
@@ -186,6 +200,34 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
   const handleNextGem = () => {
     const maxIndex = Math.min(filteredMinedGems.length, filteredSelectedGems.length) - 1;
     setCurrentGemIndex(prev => (prev === maxIndex ? 0 : prev + 1));
+  }
+
+  const renderJewelry = () => {
+    return jewelryDetails.map((jewelry, key) => (
+      <div key={key} className="card">
+        <h2>Jewelry Details</h2>
+        {jewelry.fileURL && (
+          <div>
+            <a href={jewelry.fileURL} target="_blank" rel="noopener noreferrer">
+              <img src={jewelry.fileURL} alt="Jewelry" className='details-image' />
+            </a>
+          </div>
+        )}
+        <p><strong>ID:</strong> {jewelry.id.toString()}</p>
+        <p><strong>Name:</strong> {jewelry.name}</p>
+        <p><strong>Metal:</strong> {jewelry.metal}</p>
+        <p><strong>Details:</strong> {jewelry.physicalDetails.toString()} mm</p>
+        <p><strong>Sale:</strong> {jewelry.sale.toString()}</p>
+        <p><strong>Processing:</strong> {jewelry.processing.toString()}</p>
+        <p><strong>Price:</strong> {window.web3.utils.fromWei(jewelry.price.toString(), 'Ether')} Eth</p>
+        <p><strong>Jeweler:</strong> {jewelry.jeweler}</p>
+        <p><strong>Owner:</strong> {jewelry.owner}</p>
+
+        <hr />
+        <h3>Transaction Details</h3>
+        {renderTransactionDetails(filteredJewelryEvents, jewelry.id)}
+      </div>
+    ));
   };
 
   return (
@@ -198,7 +240,7 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
         </span>
         <div className="card-container">
         {renderMinedGems()[currentGemIndex]} 
-        </div>
+      </div>
         <span className="arrow" onClick={handleNextGem}>
           →
         </span>
@@ -214,7 +256,11 @@ function JewDetails({ selectedGems, minedGems, jewelry, account, jewelryContract
         <span className="arrow" onClick={handleNextGem}>
           →
         </span>
-      </div>
+       
+        </div>
+        <div className="pt-5">
+        {renderJewelry()}
+        </div>
     </div>
   );
 }
