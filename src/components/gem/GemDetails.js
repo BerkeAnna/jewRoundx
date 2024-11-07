031a0c8585e8cdc9c9dc06358e9d15f26dee3225
BerkeAnna <berke.anna0803@gmail.com>
2024-11-07 04:24:07 +0100

    fix buttons

--- src/components/gem/GemDetails.js
+++ src/components/gem/GemDetails.js
@@ -76,7 +76,7 @@ function GemDetails({ selectedGems, minedGems, account, gemstoneSelectingContrac
   const renderSelectedGems = () => {
     return gemSelected.map((gem, key) => (
       <div key={key} className="card">
-        <h2>Selected Gem Details</h2>
+        <h2>Details of the processed gemstone</h2>
         {gem.fileURL && (
           <div>
             <a href={gem.fileURL} target="_blank" rel="noopener noreferrer">
@@ -89,7 +89,7 @@ function GemDetails({ selectedGems, minedGems, account, gemstoneSelectingContrac
         <p><strong>Carat:</strong> {gem.details.carat.toString()}</p>
         <p><strong>Type:</strong> {gem.details.gemType.toString()}</p>
         <p><strong>Color:</strong> {gem.details.color.toString()}</p>
-        <p><strong>forSale:</strong> {gem.forSale.toString()}</p>
+        <p><strong>Sale:</strong> {gem.forSale.toString()}</p>
         <p><strong>Used:</strong> {gem.used.toString()}</p>
         <p><strong>Price:</strong> {window.web3.utils.fromWei(gem.price.toString(), 'Ether')} Eth</p>
         <p><strong>Gem cutter:</strong> {gem.gemCutter}</p>
